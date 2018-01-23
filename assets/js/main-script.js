e4d1527c30a4abdd6af44a24588f7ce257cdc86b
Kaylen Travis Pillay <kaylen.student@gmail.com>
2018-01-23 12:10:12 +0200

    Booom completed - Hosted @ -> projects.ktpsolutions.co.za/booom

---
+++ assets/js/main-script.js
@@ -0,0 +1,158 @@
+var booom_data = {
+    q:{
+        color: "#cb2bdb",
+        sound: new Howl({
+            src: ['sounds/bubbles.mp3']
+        })
+    },
+    w:{
+        color: "#8f6671",
+        sound: new Howl({
+            src: ['sounds/clay.mp3']
+        })
+    },
+    e:{
+        color: "#397565",
+        sound: new Howl({
+            src: ['sounds/confetti.mp3']
+        })
+    },
+    r:{
+        color: "#d4ba47",
+        sound: new Howl({
+            src: ['sounds/corona.mp3']
+        })
+    },
+    t:{
+        color: "#d1395a",
+        sound: new Howl({
+            src: ['sounds/dotted-spiral.mp3']
+        })
+    },
+    y:{
+        color: "#3779fc",
+        sound: new Howl({
+            src: ['sounds/flash-1.mp3']
+        })
+    },
+    u:{
+        color: "#49dc6e",
+        sound: new Howl({
+            src: ['sounds/flash-2.mp3']
+        })
+    },
+    i:{
+        color: "#0758df",
+        sound: new Howl({
+            src: ['sounds/flash-3.mp3']
+        })
+    },
+    o:{
+        color: "#415279",
+        sound: new Howl({
+            src: ['sounds/glimmer.mp3']
+        })
+    },
+    p:{
+        color: "#c87b20",
+        sound: new Howl({
+            src: ['sounds/moon.mp3']
+        })
+    },
+    a:{
+        color: "#568742",
+        sound: new Howl({
+            src: ['sounds/pinwheel.mp3']
+        })
+    },
+    s:{
+        color: "#837ba0",
+        sound: new Howl({
+            src: ['sounds/piston-1.mp3']
+        })
+    },
+    d:{
+        color: "#692fa5",
+        sound: new Howl({
+            src: ['sounds/piston-2.mp3']
+        })
+    },
+    f:{
+        color: "#102e4d",
+        sound: new Howl({
+            src: ['sounds/piston-3.mp3']
+        })
+    },
+    g:{
+        color: "#666d65",
+        sound: new Howl({
+            src: ['sounds/prism-1.mp3']
+        })
+    },
+    h:{
+        color: "#e41a19",
+        sound: new Howl({
+            src: ['sounds/prism-2.mp3']
+        })
+    },
+    j:{
+        color: "#1cf4e4",
+        sound: new Howl({
+            src: ['sounds/prism-3.mp3']
+        })
+    },
+    k:{
+        color: "#2e8a75",
+        sound: new Howl({
+            src: ['sounds/splits.mp3']
+        })
+    },
+    l:{
+        color: "#8d3505",
+        sound: new Howl({
+            src: ['sounds/strike.mp3']
+        })
+    },
+    z:{
+        color: "#c8d81c",
+        sound: new Howl({
+            src: ['sounds/suspension.mp3']
+        })
+    },
+    x:{
+        color: "#cb4646",
+        sound: new Howl({
+            src: ['sounds/timer.mp3']
+        })
+    },
+    c:{
+        color: "#ffa869",
+        sound: new Howl({
+            src: ['sounds/ufo.mp3']
+        })
+    },
+    v:{
+        color: "#6d5be6",
+        sound: new Howl({
+            src: ['sounds/veil.mp3']
+        })
+    },
+    b:{
+        color: "#701645",
+        sound: new Howl({
+            src: ['sounds/wipe.mp3']
+        })
+    },
+    n:{
+        color: "#dc67b5",
+        sound: new Howl({
+            src: ['sounds/zig-zag.mp3']
+        })
+    },
+    m:{
+        color: "#ac77ab",
+        sound: new Howl({
+            src: ['sounds/squiggle.mp3']
+        })
+    },
+};
